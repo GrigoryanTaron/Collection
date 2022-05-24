7b887fba45b14b30b252aacccd2dbd45e10c2006
Taron Grigoryan <1991taron@gmail.com>
2022-05-24 23:43:57 +0400

    debug

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Collection
@@ -7,70 +8,115 @@ namespace Collection
     {
         static void Main(string[] args)
         {
-            DaysOfWeek day = new DaysOfWeek();
-            //string s = day[1];
-            Console.WriteLine(s);
-            Console.ReadLine();
-            foreach(var item in day)
+            DayOfWeek day = new DayOfWeek();
+
+
+
+            foreach (var item in day)
             {
+                Console.WriteLine($"{item}");
 
             }
+
         }
-        public class DaysOfWeek
+        public class DayOfWeek : IEnumerable
         {
             //private string[] _days = new string[7];
             List<string> _days = new List<string>();
-            
-            public DaysOfWeek()
+
+            public DayOfWeek()
             {
-                //_days[0] = "Moday";
-                //_days[1] = "Tuesday";
-                //_days[2] = "Wednesday";
-                //_days[3] = "Thursday";
-                //_days[4] = "Friday";
-                //_days[5] = "Saturday";
-                //_days[6] = "Sunday";
-                foreach (var item in _days)
+                _days.Add("Moday");
+                _days.Add("Tuesday");
+                _days.Add("Wednesday");
+                _days.Add("Thursday");
+                _days.Add("Friday");
+                _days.Add("Saturday");
+                _days.Add("Sunday");
+
+            }
+            int position = -1;
+
+            public object Current => _days;
+            public IEnumerator GetEnumerator()
+            {
+                foreach (string item in _days)
                 {
-                    _days.Add("Moday");
-                    _days.Add("Tuesday");
-                    _days.Add("Wednesday");
-                    _days.Add("Thursday");
-                    _days.Add("Friday");
-                    _days.Add("Saturday");
-
-
+                     return GetEnumerator();
                 }
+               
+            }
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
             }
 
-            //public string this[int index]
-            //{
-            //    get
 
-            //    {
-            //        if (index == 0)
-            //            return "Moday";
-            //        if (index == 1)
-            //            return "Tuesday";
-            //        if (index == 2)
-            //            return "Wednesday";
-            //        if (index == 3)
-            //            return "Thursday";
-            //        if (index == 4)
-            //            return "Friday";
-            //        if (index == 5)
-            //            return "Saturday";
-            //        if (index == 6)
-            //            return "Sunday";
+            public bool MoveNext()
+            {
+                position++;
+                return position < _days.Count;
+            }
 
-            //        else
-            //            return "hello";
-            //    }
-           // }
+            public void Reset()
+            {
+                position = -1;
+            }
 
 
         }
+        //
+        // Summary:
+        //     Exposes the enumerator, which supports a simple iteration over a collection of
+        //     a specified type.
+        //
+        // Type parameters:
+        //   T:
+        //     The type of objects to enumerate.
+        public interface IEnumerable : IEnumerator
+        {
+            //
+            // Summary:
+            //     Returns an enumerator that iterates through the collection.
+            //
+            // Returns:
+            //     An enumerator that can be used to iterate through the collection.
+            IEnumerator GetEnumerator();
+        }
+        public interface IEnumerator
+        {
+            //
+            // Summary:
+            //     Gets the element in the collection at the current position of the enumerator.
+            //
+            // Returns:
+            //     The element in the collection at the current position of the enumerator.
+            object Current { get; }
+
+            //
+            // Summary:
+            //     Advances the enumerator to the next element of the collection.
+            //
+            // Returns:
+            //     true if the enumerator was successfully advanced to the next element; false if
+            //     the enumerator has passed the end of the collection.
+            //
+            // Exceptions:
+            //   T:System.InvalidOperationException:
+            //     The collection was modified after the enumerator was created.
+            bool MoveNext();
+            //
+            // Summary:
+            //     Sets the enumerator to its initial position, which is before the first element
+            //     in the collection.
+            //
+            // Exceptions:
+            //   T:System.InvalidOperationException:
+            //     The collection was modified after the enumerator was created.
+            void Reset();
+        }
     }
+
 
 }
 
